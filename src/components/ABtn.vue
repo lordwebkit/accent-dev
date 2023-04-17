e130f90555ac253840715c05d9c913bcb76d1587
Kashirskiy Nikita <lordwebkit@gmail.com>
2023-04-17 19:55:12 +0300

    add checkbox, button components

---
+++ src/components/ABtn.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="a-btn">
+      <slot></slot>
+    </button>
+  </template>
+  <style lang="scss" scoped>
+  .a-btn {
+    width: 100%;
+    background: #2dc574;
+    border: 0.1rem solid transparent;
+    border-radius: 4px;
+    color: #fff;
+    cursor: pointer;
+    display: inline-block;
+    font-family: inherit;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 1.1;
+    outline: none;
+    padding: 14px;
+    text-align: center;
+    transition: all .3s;
+    user-select: none;
+    white-space: nowrap;
+    &:hover {
+      background: #26a763;
+    }
+    &:active {
+      background: #229457;
+    }
+  }
+  </style>
---
+++ src/components/ACheckbox.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="checkbox-wrapper">
+      <input type="checkbox" class="checkbox" :id="checkboxId" :checked="checkboxChecked">
+      <label class="checkbox" :for="checkboxId" @click.prevent="setCheckbox"></label>
+    </div>
+  </template>
+  <script>
+  export default {
+    props: ['checkboxId', 'checkboxChecked', 'setCheckbox']
+  }
+  </script>
+  <style lang="scss" scoped>
+  .checkbox {
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+  }
+  .checkbox-wrapper {
+    display: inline-block;
+  }
+  .checkbox+label {
+    position: relative;
+    border: 1px solid #2dc574;
+    border-radius: 0.2px;
+    height: 18px;
+    width: 18px;
+    cursor: pointer;
+    margin-right: 10px;
+    display: inline-block;
+    opacity: 1;
+    z-index: 1;
+  }
+  .checkbox:checked+label {
+    background: #2dc574 url("@/assets/icons/checked.svg") 50% 30% no-repeat;
+  }
+  </style>
