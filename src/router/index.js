9573b674137faf4f4136eed778e80db2ec7dea05
Kashirskiy Nikita <lordwebkit@gmail.com>
2023-04-17 19:56:22 +0300

    add routing for cart

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ShopView from '../views/ShopView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'shop',
+      component: ShopView
     },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('@/views/CartView.vue')
+    }
   ]
 })
 
